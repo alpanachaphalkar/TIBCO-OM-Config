da0d75d61f214962148c566bc37cf0a7f43547fa
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-10-27 13:07:53 +0200

    okteto setup for hf7 with ldaps

--- scripts/create_users.py
+++ scripts/create_users.py
@@ -3,6 +3,7 @@
 import requests
 import environment as env
 import logging.config
+from users import users
 
 logging.config.fileConfig('logging.ini')
 logger = logging.getLogger('post-install')
@@ -11,54 +12,45 @@ authorization_service_url = f"{env.authorization_svc}/v1/user"
 
 def is_user_created(response: requests.Response, user: str) -> None:
     if response.status_code == 201:
-        logger.info(f"Created User: {user}")
+        logger.info(f"{'* ' * 15} Created User: {user} {'* ' * 15}")
     else:
         logger.error(f"Something went wrong while creating user: {user}")
         raise Exception(f"{response.__dict__}")
 
 
-headers = {
-    'Content-Type': 'application/json;charset=utf-8',
-    'X-API-Key': env.x_api_key,
-    'X-API-AppId': env.x_api_appid
-}
-
-admin_user_payload = {
-    "user": [
-        {
-            "enabled": True,
-            "password": env.om_admin_password,
-            "tenantId": env.tenant_id,
-            "userName": env.om_admin_username,
-            "userRoles": [
-                "ROLE_ADMIN"
-            ]
-        }
-    ]
-}
-
-api_user_payload = {
-    "user": [
-        {
-            "enabled": True,
-            "password": env.om_apiuser_password,
-            "tenantId": env.tenant_id,
-            "userName": env.om_apiuser_username,
-            "userRoles": [
-                "ROLE_ADMIN"
-            ]
-        }
-    ]
-}
+def get_user_req_payload(tenant: str, user: str, password: str, roles: list[str]) -> dict:
+    return {
+        "user": [
+            {
+                "enabled": True,
+                "password": password,
+                "tenantId": tenant,
+                "userName": user,
+                "userRoles": roles
+            }
+        ]
+    }
 
 
-def create_user(user: str, req_body: dict) -> requests.Response:
-    logger.info(f"Creating User: {user}")
+def create_user(tenant: str, user: str, password: str, roles: list[str]) -> requests.Response:
+    logger.info(f"{'* ' * 15} Creating User: {user} {'* ' * 15}")
+
+    headers = {
+        'Content-Type': 'application/json;charset=utf-8',
+        'X-API-Key': env.x_api_key,
+        'X-API-AppId': env.x_api_appid
+    }
+
+    req_body = get_user_req_payload(tenant=tenant, user=user, password=password, roles=roles)
+
+    logger.info(f"URL: {authorization_service_url}")
+    logger.info(f"User: {user}")
+    logger.info(f"Tenant ID: {tenant}")
+    logger.info(f"Roles: {roles}")
     return requests.post(authorization_service_url, headers=headers, json=req_body, verify=False)
 
 
-admin_response = create_user(user=env.om_admin_username, req_body=admin_user_payload)
-is_user_created(response=admin_response, user=env.om_admin_username)
-
-api_user_response = create_user(user=env.om_apiuser_username, req_body=api_user_payload)
-is_user_created(response=api_user_response, user=env.om_apiuser_username)
+for u in users:
+    create_user_response = create_user(tenant=u["tenant"], user=u["user"],
+                                       password=u["password"], roles=u["roles"])
+    is_user_created(response=create_user_response, user=u["user"])
---
+++ scripts/users.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import environment as env
+
+users = [
+    {
+        "user": env.om_admin_username,
+        "password": "",
+        "tenant": env.tenant_id,
+        "roles": [
+            "AZ_TIBCO_OM_NONPROD_ADMIN"
+        ]
+    },
+    {
+        "user": env.om_apiuser_username,
+        "password": "",
+        "tenant": env.tenant_id,
+        "roles": [
+            "AZ_TIBCO_OM_NONPROD_ADMIN"
+        ]
+    },
+    {
+        "user": "newuser@alpsintech.com",
+        "password": "",
+        "tenant": env.tenant_id,
+        "roles": [
+            "AZ_TIBCO_OM_NONPROD_USER"
+        ]
+    },
+    {
+        "user": "first.last@alpsintech.com",
+        "password": "",
+        "tenant": env.tenant_id,
+        "roles": [
+            "AZ_TIBCO_OM_NONPROD_USER"
+        ]
+    }
+]
